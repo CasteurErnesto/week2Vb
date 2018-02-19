c1ac4c5d6f121f3d89bfca8fed2a212b9cdf36ca
ernestoCasteur <ernesto.casteur@student.howest.be>
2018-02-19 10:06:48 +0100

    use of static files

--- week2Vb/Startup.cs
+++ week2Vb/Startup.cs
@@ -25,6 +25,8 @@ namespace week2Vb
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
